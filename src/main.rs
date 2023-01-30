b86decc2499449763c78845c62c2703130ba9f2f
t-edzuka <t.edzuka@happy-quality.jp>
2023-01-30 11:43:53 +0900

    Add ignore main.rs file for tarpaulin coverage.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::net::TcpListener;
 
 const PORT: &str = "8080";
 
+#[cfg(not(tarpaulin_include))]
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let app_addr = format!("127.0.0.1:{PORT}");
